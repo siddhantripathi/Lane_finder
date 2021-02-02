abf29a476aec3d3014e6bca8b0aae9954b6e506d
Siddhant Tripathi <60621886+siddhantripathi@users.noreply.github.com>
2021-02-02 15:18:21 +0530

    Update lane_detection.py

--- lane_detection.py
+++ lane_detection.py
@@ -1,108 +1,59 @@
+import matplotlib.pylab as plt
 import cv2
 import numpy as np
- 
 
-def canny(img):
-    
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    
-    kernel = 5
-    blur = cv2.GaussianBlur(gray,(kernel, kernel),0)
-    
-    canny = cv2.Canny(gray, 50, 150)
-    return canny
-
-
-def region_of_interest(canny):
-   
-    height = canny.shape[0]
-    
-    width = canny.shape[1]
-    
-    mask = np.zeros_like(canny)
-    
-    triangle = np.array([[
-    (200, height),
-    (550, 250),
-    (1100, height),]], np.int32)
-   
-    cv2.fillPoly(mask, triangle, 255)
-    
-    masked_image = cv2.bitwise_and(canny, mask)
+def region_of_interest(img, vertices):
+    mask = np.zeros_like(img)
+    #channel_count = img.shape[2]
+    match_mask_color = 255
+    cv2.fillPoly(mask, vertices, match_mask_color)
+    masked_image = cv2.bitwise_and(img, mask)
     return masked_image
 
+def draw_the_lines(img, lines):
+    img = np.copy(img)
+    blank_image = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
 
-def houghLines(cropped_canny):
-    return cv2.HoughLinesP(cropped_canny, 2, np.pi/180, 100, 
-        np.array([]), minLineLength=40, maxLineGap=5)
-
-
-def addWeighted(frame, line_image):
-    return cv2.addWeighted(frame, 0.8, line_image, 1, 1)
- 
-
-def display_lines(img,lines):
-   
-    line_image = np.zeros_like(img)
-    if lines is not None:
-        
-        for line in lines:
-            for x1, y1, x2, y2 in line:
-                
-                cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
-    return line_image
-
-# 
-def make_points(image, line):
-    slope, intercept = line
-    y1 = int(image.shape[0])
-    y2 = int(y1*2.5/5)      
-    x1 = int((y1 - intercept)/slope)
-    x2 = int((y2 - intercept)/slope)
-    return [[x1, y1, x2, y2]]
- 
-
-def average_slope_intercept(image, lines):
-   
-    left_fit    = []
-    right_fit   = []
-    if lines is None:
-        return None
     for line in lines:
-        
         for x1, y1, x2, y2 in line:
-            
-            fit = np.polyfit((x1,x2), (y1,y2), 1)
-            slope = fit[0]
-            intercept = fit[1]
-            
-            left_fit.append((slope, intercept))
-    else:
-                
-        right_fit.append((slope, intercept))
-     
-    left_fit_average  = np.average(left_fit, axis=0)
-    right_fit_average = np.average(right_fit, axis=0)
-    
-    left_line  = make_points(image, left_fit_average)
-    right_line = make_points(image, right_fit_average)
-    
-    averaged_lines = [left_line, right_line]
-    return averaged_lines
+            cv2.line(blank_image, (x1,y1), (x2,y2), (0, 255, 0), thickness=10)
 
-cap = cv2.VideoCapture("test2.mp4")
-while(cap.isOpened()):
-    _, frame = cap.read()
-    canny_image = canny(frame)
-    cropped_canny = region_of_interest(canny_image)
-    lines = houghLines(cropped_canny)
-    averaged_lines = average_slope_intercept(frame, lines)
-    line_image = display_lines(frame, averaged_lines)
-    combo_image = addWeighted(frame, line_image)
-    cv2.imshow("result", combo_image)
-    
+    img = cv2.addWeighted(img, 0.8, blank_image, 1, 0.0)
+    return img
+
+# = cv2.imread('road.jpg')
+#image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+def process(image):
+    #print(image.shape)
+    height = image.shape[0]
+    width = image.shape[1]
+    region_of_interest_vertices = [
+        (0, height),
+        (width/2, height/2),
+        (width, height)
+    ]
+    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    canny_image = cv2.Canny(gray_image, 100, 120)
+    cropped_image = region_of_interest(canny_image,
+                    np.array([region_of_interest_vertices], np.int32),)
+    lines = cv2.HoughLinesP(cropped_image,
+                            rho=2,
+                            theta=np.pi/180,
+                            threshold=50,
+                            lines=np.array([]),
+                            minLineLength=40,
+                            maxLineGap=100)
+    image_with_lines = draw_the_lines(image, lines)
+    return image_with_lines
+
+cap = cv2.VideoCapture(r'test_videos/test2.mp4')
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    frame = process(frame)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() 
